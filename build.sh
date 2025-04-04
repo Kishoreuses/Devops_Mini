7c6aeba2011ea3ce7f4e53cf08350f2d8d5943da
Kishoreuses <134022020+Kishoreuses@users.noreply.github.com>
2025-04-04 23:22:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t alumni_student .
+docker build -t alumniStudent .
 echo Hyyy
 docker login -u kishores572004 -p 05-Jul-04
-docker tag alumni_student kishores572004/alumni_student
+docker tag alumniStudent kishores572004/alumni_student
 docker push kishores572004/alumni_student
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
